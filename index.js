b6e1a952b8f9f9f817b2f301081473f5a83137a2
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-31 20:34:49 +0600

    js to json and json to js conversion

--- index.js
+++ index.js
@@ -14,9 +14,37 @@
 
 
 //json data access using loop
-const data = require("./students_data.js");
+// const data = require("./students_data.js");
 
-for(x in data){
-  console.log(x);
-  console.log(data[x]);
+// for(x in data){
+//   console.log(x);
+//   console.log(data[x]);
+// }
+
+
+
+//convert js data to json object
+
+const data = {
+  name:"mithun",
+  age:25
 }
+
+console.log(JSON.stringify(data));
+
+//json data convert to js data
+const data2 ={
+  "friends": [
+    {
+      "name":"Alamin",
+      "age":27
+    },
+    {
+      "name":"Mridul",
+      "age":27
+    }
+]
+
+
+}
+console.log(JSON.parse(data2));
