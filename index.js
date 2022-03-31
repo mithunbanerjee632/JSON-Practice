c1b090f4b597954976e2ea2b89d5ccfd6d0d2a55
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-03-31 20:21:07 +0600

    json data access using loop

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const data = require("./friends.js");
+//const data = require("./friends.js");
 //console.log(data);
 //console.log(data.friends[0]);
 //console.log(data.friends[0].name);
 
 //modify
-data.friends[0].name ="Mithun";
+// data.friends[0].name ="Mithun";
 
-console.log(data.friends[0].name);
+// console.log(data.friends[0].name);
 
-//delete
-delete data.friends[0].name;
-console.log(data);
+// //delete
+// delete data.friends[0].name;
+// console.log(data);
 
+
+//json data access using loop
+const data = require("./students_data.js");
+
+for(x in data){
+  console.log(x);
+  console.log(data[x]);
+}
